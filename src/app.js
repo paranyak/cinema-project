d64f9bda317c56f77f90e2a6c489ed8f484603f7
Olha Korabliova <korablyova@ucu.edu.ua>
2018-02-21 22:16:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/common.less"
 import MovieLayout from "./components/MovieLayout";
+// import HomeLayout from "./components/HomeLayout";
+
 ReactDOM.render(
   <MovieLayout/>,
   document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <HomeLayout/>,
+//   document.getElementById("root")
+// );
