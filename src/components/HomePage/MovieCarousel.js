24641e341b99e4e41719b1ee3dd3e6f3c4d2050c
OlyaKorabliova <korablyova@ucu.edu.ua>
2018-03-18 20:12:11 +0200

    Added functionality to scroll buttons.

--- src/components/HomePage/MovieCarousel.js
+++ src/components/HomePage/MovieCarousel.js
@@ -5,18 +5,78 @@ import {connect} from "react-redux";
 import {Link} from 'react-router-dom'
 import MoviePoster from "./MoviePoster";
 import {getAllMovies} from '../../reducers';
+import throttle from 'lodash';
 
 const b = block("MovieCarousel");
 
+const scrollTo = (params) => {
+    const {
+        el,
+        to,
+        duration,
+        scrollDir
+    } = params;
+    const start = el[scrollDir];
+    const change = to - start;
+    const increment = 20;
+
+    const animateScroll = (elapsedTime) => {
+        elapsedTime += increment;
+        el[scrollDir] = easeInOut(elapsedTime, start, change, duration);
+        if (elapsedTime < duration) {
+            setTimeout(() => {
+                animateScroll(elapsedTime);
+            }, increment);
+        }
+    };
+    animateScroll(0);
+};
+
+const easeInOut = (curTime, start, change, duration) => {
+    curTime /= duration / 2;
+    if (curTime < 1) {
+        return change / 2 * curTime * curTime + start;
+    }
+    curTime -= 1;
+    return -change / 2 * (curTime * (curTime - 2) - 1) + start;
+};
+
 class MovieCarousel extends Component {
+
+    handleClick(k = 1) {
+        const {carousel} = this.refs;
+        let numOfItemsToScroll = 3.5;
+        let widthOfItem = 275;
+        let newPos = carousel.scrollLeft + k * (widthOfItem * numOfItemsToScroll);
+        const timeForItem = 200;
+        const totalTime = numOfItemsToScroll * timeForItem;
+        scrollTo({
+            el: carousel,
+            to: newPos,
+            duration: totalTime,
+            scrollDir: 'scrollLeft'
+        });
+    }
+
+    leftClick() {
+        this.handleClick(-1)
+    };
+
+    rightClick() {
+        this.handleClick();
+    };
+
     render() {
         const {label, films} = this.props;
         return (
             <section className={b()}>
-                <button className={b('button')}>
+                <button
+                    className={b('button')}
+                    onClick={this.leftClick.bind(this)}
+                >
                     <span className={b('icon', ['left'])}></span>
                 </button>
-                <div className={b('movies')}>
+                <div ref='carousel' className={b('movies')}>
                     {films
                         .filter(film => film.label === label)
                         .map(film =>
@@ -25,7 +85,10 @@ class MovieCarousel extends Component {
                             </Link>
                         )}
                 </div>
-                <button className={b('button')}>
+                <button
+                    className={b('button')}
+                    onClick={this.rightClick.bind(this)}
+                >
                     <span className={b('icon', ['right'])}></span>
                 </button>
             </section>
