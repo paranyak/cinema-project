9992e62fb1cee0b7e3852741b14f990bf285e933
olya <korablyova@ucu.edu.ua>
2018-05-07 13:46:05 +0300

    Added new get request.

--- server/routes/actors.js
+++ server/routes/actors.js
@@ -3,47 +3,52 @@ var Actor = models.Actor;
 var express = require('express');
 var router = express.Router();
 
-router.get('/byId/:id', function(req, res) {
-  const id = req.params.id;
-  Actor.findById(id, function (err, actor) {
-      res.send(actor);
-  });
+router.get('/byId/:id', function (req, res) {
+    const id = req.params.id;
+    Actor.findById(id, function (err, actor) {
+        res.send(actor);
+    });
+});
+router.get('/bySlugName/:slugName', function (req, res) {
+    const slugName = req.params.slugName;
+    Actor.find({slugName}, (err, actor) => res.send(actor));
 });
 
-router.get('/ids', function(req, res) {
-  let params = {};
-  let query = req.query;
-  let dbQuery;
-  // if (query['_label']) {
-  //   params.label = query['_label'];
-  // }
-  dbQuery = Actor.find(params).select('id');
-  if (query['_limit']) {
-    let page = +query['_page'] || 1;
-    let limit = +query['_limit'];
-    dbQuery = dbQuery.limit(limit).skip((page - 1) * limit);
-  }
+
+router.get('/ids', function (req, res) {
+    let params = {};
+    let query = req.query;
+    let dbQuery;
+    // if (query['_label']) {
+    //   params.label = query['_label'];
+    // }
+    dbQuery = Actor.find(params).select('id');
+    if (query['_limit']) {
+        let page = +query['_page'] || 1;
+        let limit = +query['_limit'];
+        dbQuery = dbQuery.limit(limit).skip((page - 1) * limit);
+    }
 
 
-  dbQuery.exec(function(err, actor){
-    console.log(err);
-    actor = actor || [];
-    res.send(actor.map(el => el['_id']));
-  })
+    dbQuery.exec(function (err, actor) {
+        console.log(err);
+        actor = actor || [];
+        res.send(actor.map(el => el['_id']));
+    })
 });
 
-router.post('/', function(req, res) {
-  console.log("++++++++++++++++++++++++");
-  new Actor(req.body).save().then(function(actor) {
-    res.send(actor)
-  });
+router.post('/', function (req, res) {
+    console.log("++++++++++++++++++++++++");
+    new Actor(req.body).save().then(function (actor) {
+        res.send(actor)
+    });
 })
 
-router.patch('/:id', function(req, res) {
-  Actor.findByIdAndUpdate({ _id: req.params.id }, { $set: req.body})
-    .then(function(actor) {
-      res.send(actor)
-    });
+router.patch('/:id', function (req, res) {
+    Actor.findByIdAndUpdate({_id: req.params.id}, {$set: req.body})
+        .then(function (actor) {
+            res.send(actor)
+        });
 })
 
 module.exports = router;
