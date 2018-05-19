4ab2e40bf448b73ac8381c6c406c8f90f587967a
olya <korablyova@ucu.edu.ua>
2018-05-19 13:55:45 +0300

    Changes in server.js.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var db = require('./db');
-var movies = require('./routes/movies');
-var actors = require('./routes/actors');
-var app = express();
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require('express');
+const db = require('./db');
+const movies = require('./routes/movies');
+const actors = require('./routes/actors');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 const url = 'mongodb://olya:1@ds227740.mlab.com:27740/cinema-project';
 
 const port = process.env.PORT || 3000;
 
-app.use(cors({origin: 'https://csucu-cinema-project.herokuapp.com'}));
+// app.use(cors({origin: 'https://csucu-cinema-project.herokuapp.com'}));
 // app.use(cors({origin: 'http://localhost:5000'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/movies', movies);
 app.use('/actors', actors);
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.use("/*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
 
 db.connect(url, (err) => {
     if (err) {
