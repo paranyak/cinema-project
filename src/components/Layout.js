a2628a2b665da6001de1b56e42ce306726008f63
mykhailoivankiv <neformal.lviv@gmail,com>
2018-06-13 23:00:05 +0300

    Refactor Layout component.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,67 +23,65 @@ import EditActorPage from "./EditActorPage"
 import AddActorLayout from "./AddActorLayout"
 
 import firebase from "../api/auth"
+import { compose, propOr } from "ramda"
+import { lifecycle } from "recompose"
 
 const b = block("Layout")
 
-class Layout extends Component {
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.props.setUser(user)
-      if (user) {
-        this.props.userAdditionalInfo(user.uid)
-      }
-    })
-  }
-
-  render() {
-    let role = ""
-    if (this.props.user) {
-      role = this.props.user.role
+const PrivateRoute = ({ component: Component, role, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      role === "admin" ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      )
     }
-    const PrivateRoute = ({ component: Component, ...rest }) => (
-      <Route
-        {...rest}
-        render={props =>
-          role === "admin" ? (
-            <Component {...props} />
-          ) : (
-            <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
-          )
-        }
-      />
-    )
-    return (
-      <div className={b()}>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={HomeLayout} />
-          <Route path="/movie/:slug" component={MovieLayout} />
-          <Route path="/schedule/:day?" component={ScheduleLayout} />
-          <Route path="/allactors" component={AllActors} />
-          <Route path="/actor/:slug" component={ActorLayout} />
-          <PrivateRoute path="/add-movie" component={AddMovieLayout} />
-          <PrivateRoute path="/add-actor" component={AddActorLayout} />
-          <PrivateRoute path="/edit-movie/:slug" component={EditMoviePage} />
-          <PrivateRoute path="/edit-actor/:slug" component={EditActorPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
-        </Switch>
-      </div>
-    )
-  }
-}
-
-Layout = withRouter(
-  connect(
-    (state, props) => ({
-      user: getCurrentUser(state),
-    }),
-    dispatch => ({
-      setUser: user => dispatch(setUser(user)),
-      userAdditionalInfo: id => dispatch(userAdditionalInfo(id)),
-    }),
-  )(Layout),
+  />
 )
 
-export default Layout
+const Layout = ({ user }) => {
+  let role = propOr("", "role", user)
+
+  return (
+    <div className={b()}>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={HomeLayout} />
+        <Route path="/movie/:slug" component={MovieLayout} />
+        <Route path="/schedule/:day?" component={ScheduleLayout} />
+        <Route path="/allactors" component={AllActors} />
+        <Route path="/actor/:slug" component={ActorLayout} />
+        <PrivateRoute role={role} path="/add-movie" component={AddMovieLayout} />
+        <PrivateRoute role={role} path="/add-actor" component={AddActorLayout} />
+        <PrivateRoute role={role} path="/edit-movie/:slug" component={EditMoviePage} />
+        <PrivateRoute role={role} path="/edit-actor/:slug" component={EditActorPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </div>
+  )
+}
+
+const enhance = compose(
+  withRouter,
+
+  connect(state => ({ user: getCurrentUser(state) }), {
+    setUser,
+    userAdditionalInfo,
+  }),
+
+  lifecycle({
+    componentWillMount() {
+      const { setUser, userAdditionalInfo } = this.props
+
+      firebase.auth().onAuthStateChanged(user => {
+        setUser(user)
+        if (user) userAdditionalInfo(user.uid)
+      })
+    },
+  }),
+)
+
+export default enhance(Layout)
